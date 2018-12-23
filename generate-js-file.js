6257e2039cc8f67652471c0f00045292b86b4460
varzager <varzager@gmail.com>
2018-12-23 17:53:32 +0200

    #EE-15787| testing git-blame

--- generate-js-file.js
+++ generate-js-file.js
@@ -1,13 +1,15 @@
 const loadJsonFile = require('load-json-file');
+var gitBlame = require('git-blame');
 const fs = require('fs');
 const util = require('util');
+var path = require('path');
+
+
+const outPutDir = './dist';
+const outputFilePath = `${outPutDir}/temp.ts`;
 
 
 class TSClassBuilder {
-
-    giveClassName() {
-        return '';
-    }
 
     createTsClassStringMember(keyProp, realKey) {
         return `    public ${keyProp} : string =  '${realKey}';\n`;
@@ -32,7 +34,7 @@ class TSClassBuilder {
 }
 
 
-async function generateJsFile() {
+function getArgs() {
     let [, , args] = process.argv;
 
     args = Array.isArray(args) ? args : [args];
@@ -42,9 +44,13 @@ async function generateJsFile() {
         return {[key.slice(2)]: value};
     }));
 
+    return {input}
+}
+
+async function generateJsFile(input) {
     const localeKeys = await loadJsonFile(input).then(localeKeys => {
         const final = {};
-        Object.keys(localeKeys).forEach((key) => final[key.replace(/\./g,'_')] = key);
+        Object.keys(localeKeys).forEach((key) => final[key.replace(/\./g, '_')] = key);
         return final;
     });
 
@@ -52,13 +58,35 @@ async function generateJsFile() {
 
     const finalClass = await tsClassBuilder.get(localeKeys);
 
-    var outPutDir = './dist';
 
-    if (!fs.existsSync(outPutDir)){
+    if (!fs.existsSync(outPutDir)) {
         fs.mkdirSync(outPutDir);
     }
 
-    fs.writeFileSync(`${outPutDir}/temp.ts`, finalClass, 'utf-8');
+    fs.writeFileSync(outputFilePath, finalClass, 'utf-8');
 }
 
-generateJsFile();
+function analyzeUsage(input) {
+
+    var repoPath = path.resolve(('.git'));
+    var file = outputFilePath;
+
+    gitBlame(repoPath, {
+        file: input,
+    }).on('data', function (type, data) {
+        // type can be 'line' or 'commit'
+        if(type === 'commit') {
+            console.log(type, data);
+        }
+    }).on('error', function (err) {
+        console.error(err.message);
+        process.exit(1);
+    }).on('end', function () {
+        console.log('±±±±±±±±±±±±±±±±±±');
+        console.log("That's all, folks!");
+    });
+}
+
+const {input} = getArgs();
+generateJsFile(input);
+analyzeUsage(input);
