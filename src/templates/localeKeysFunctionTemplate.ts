afe5c1a3e2cd8fc2609ae0eb1e6ae20ae3b9cd35
varzager <varzager@gmail.com>
2020-07-09 12:26:21 +0300

    add /* eslint-disable */ to file

--- src/templates/localeKeysFunctionTemplate.ts
+++ src/templates/localeKeysFunctionTemplate.ts
@@ -1,7 +1,9 @@
 /* tslint:disable */
+/* eslint-disable */
 export type ILocaleKeysTemplate = ReturnType<typeof LocaleKeysTemplate>;
 
 export function LocaleKeysTemplate(/* constructor args */) {
     return /* placeholder: keys here */;
 }
+/* eslint-enable  */
 /* tslint:enable */
