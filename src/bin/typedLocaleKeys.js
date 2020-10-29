43dc074937295442b9c127b2425a60895f902831
zeevg <zeevg@wix.com>
2020-10-29 13:30:05 +0200

    fixes

--- src/bin/typedLocaleKeys.js
+++ src/bin/typedLocaleKeys.js
@@ -13,7 +13,7 @@ program
     .option('-t, --translate [bool]', 'should add translate function. NOTE: will wrap value with function', program.BOOLEAN, true, false)
     .option('--showTranslations [bool]', 'add translations as function\'s comment', program.BOOLEAN, true, false)
     .option('--functionName [name]', 'Generated function name', program.STRING, 'LocaleKeys', false)
-    .option('--singleCurlyBraces [bool]', 'Use single curly braces', program.BOOL, undefined, false)
+    .option('--singleCurlyBraces [bool]', 'Read interpolation arguments using single curly instead of of double', program.BOOL, undefined, false)
     .action(async ({source}, {output, functionName, nested, translate, showTranslations, singleCurlyBraces}) => {
         const entryFilesByFunctionName = {};
         const packageJSON = await loadJsonFile('package.json');
