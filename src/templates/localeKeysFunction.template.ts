31f3ca62479ada600b85e38add0e92931fa7823e
Tit Kovalenko <t.a.kvlnk@gmail.com>
2021-08-31 00:27:58 +0300

    feat: align return type with i18next return types

--- src/templates/localeKeysFunction.template.ts
+++ src/templates/localeKeysFunction.template.ts
@@ -3,7 +3,7 @@
 /* placeholder: react import here */
 export type ILocaleKeysTemplate = ReturnType<typeof LocaleKeysTemplate>;
 
-type AbstractTranslateFunction = (...args: any[]) => string;
+type AbstractTranslateFunction = (...args: any[]) => string | object | Array<string | object> | undefined | null;
 
 export function LocaleKeysTemplate/* constructor type args */(/* constructor args */) {
     return /* placeholder: keys here */;
