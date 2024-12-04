0b55cfd429d355e8ce651d43b1c01d7ff63f03db
Tit Kovalenko <titk@wix.com>
2024-12-04 09:49:17 +0200

    revert new behavior forcing

--- src/Generator.ts
+++ src/Generator.ts
@@ -80,7 +80,6 @@ export class Generator {
       translationFn: this.translateFn,
       typeName: this.typeName,
       resultFile,
-      proxyImplementation: true,
     }).write();
 
     if (this.options.reactBindings) {
