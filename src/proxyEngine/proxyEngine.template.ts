c6898a608d4bb3bd942337170f26a705416143e2
guyw <guyw@wix.com>
2025-01-04 20:12:34 +0200

    chore: adding `pathgen` instead of implementing on our own

--- src/proxyEngine/proxyEngine.template.ts
+++ src/proxyEngine/proxyEngine.template.ts
@@ -1,15 +1,10 @@
+import { pathgen } from 'object-path-generator';
+
 const __proxyImplName__ = <R extends string>(
-  t = (...[k]: unknown[]) => k as R,
-  prevKeys = ''
-): unknown =>
-  new Proxy((...args: unknown[]) => t(prevKeys, ...args), {
-    get: (_, key: string): unknown => {
-      let nextKey = prevKeys;
-
-      if (key !== '__ownValueAlias__') {
-        nextKey = prevKeys ? [prevKeys, key].join('.') : key;
-      }
-
-      return __proxyImplName__(t, nextKey);
-    },
-  });
+  t = (...[k]: unknown[]) => k as R
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+) =>
+  pathgen<R>(undefined, (path: string, ...options: any[]) => {
+    const finalPath = path.split('.__ownValueAlias__')[0];
+    return t(finalPath, ...options);
+  }) as unknown;
